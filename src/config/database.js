f7a70b663725b1ae9592daf43c792f83cd1ad02a
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2020-02-26 21:43:00 -0300

    Primeiro Commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'carloseduardo',
+  password: 'Abcdefghijklmn@0123456789',
+  database: 'tasklist',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
